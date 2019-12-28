04e277b9f63ce834adfd868d5d8df8de2e47c7a7
nrootconauto <nrootconauto@gmail.com>
2019-12-28 20:45:04 +0000

    changed example

--- src/test.cpp
+++ src/test.cpp
@@ -5,9 +5,8 @@
 		binaryVector::binaryVector<unsigned char> vec(8*3);
 		vec.write(0,0b11001101);
 		binaryVector::binaryVectorView<unsigned char> view(vec,2);
-		vec<<=9;
-		std::cout<<"ReaD:"<<(unsigned int)(view.read(0))<<std::endl;
-		std::cout<<"Read2:"<<(unsigned int )(view.read(1))<<std::endl;
+		vec<<=10;
+		std::cout<<"Hello World zero:"<<vec<<std::endl;
 		view.write(0,0b11011011);
 		view<<=2;
 		std::cout<<"Hello World one:"<<vec<<std::endl;
